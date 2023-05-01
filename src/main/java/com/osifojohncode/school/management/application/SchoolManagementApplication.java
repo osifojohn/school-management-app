ef4fe2b3b35d9179d2a1f035ba516279dd521487
Osifo John <osifojohntec@gmail.com>
2023-05-01 21:41:16 +0100

    first commit

---
+++ src/main/java/com/osifojohncode/school/management/application/SchoolManagementApplication.java
@@ -0,0 +1,13 @@
+package com.osifojohncode.school.management.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osifojohncode/school/management/application/SchoolManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osifojohncode.school.management.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
