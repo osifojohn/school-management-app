8e735374092504f490a0da6d1d632cbaa29d7729
Osifo John <osifojohntec@gmail.com>
2023-05-03 22:30:09 +0100

    updated unit test for one to one relationship

--- src/test/java/com/osifojohncode/school/management/application/repository/CourseRepositoryTest.java
+++ src/test/java/com/osifojohncode/school/management/application/repository/CourseRepositoryTest.java
@@ -1,7 +1,24 @@
 package com.osifojohncode.school.management.application.repository;
 
+import com.osifojohncode.school.management.application.entity.Course;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CourseRepositoryTest {
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Test
+    public void printCourse(){
+        List<Course> courses =
+                courseRepository.findAll();
+        System.out.println("courses= " + courses);
+    }
 
 }
